c06d6586a64213f38b21ad33f8fdb6706dabbe07
The Caravan <theCaravan@users.noreply.github.com>
2016-05-06 10:06:23 -0500

    Version 1.5.1
    Deleted one connection test that keeps failing
    Organized the connection tests into three parts. If all pass, then the internet must be connected

--- internetConnection.sh
+++ internetConnection.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo -e "\ninternetConnection script 1.4 - By the Caravan Systems\n"
+echo -e "\ninternetConnection script 1.5.1 - By the Caravan Systems\n"
 
 currTime=$(date +%Y-%m-%d~%T~%Z)
 logLoc="/home/sb5060tx/Logs/internetConnection-sh/$currTime"
@@ -12,31 +12,48 @@ mkdir -v $logLoc
 echo -e "\n--------------------------------------------"
 
 echo -e "Checking if we are connected to the internet\n"
-x=`iw dev wlp2s0 link`
 
-wget -q --spider http://google.com &> $logLoc"/spider.txt"
+###################################################
 
-if [ $? -eq 0 ]; then
-    spider="yes"
-else
-    spider="no"
-fi
-
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1 &> $logLoc"/getEcho.txt"
+echo -e "\nRunning Test 1 of 3"
+timeout --preserve-status 4 wget -q --spider http://google.com &> $logLoc"/testOne.txt"
 
 if [ $? -eq 0 ]; then
-    get="yes"
+    testOne="Pass"
 else
-    get="no"
+    testOne="Fail"
+fi
+echo -e "Finish Test 1 of 3 - Result is $testOne"
+
+###################################################
+
+echo -e "\nRunning Test 2 of 3"
+timeout --preserve-status 4 ping -vW 3 -w 3 www.google.com &> $logLoc"/testTwo.txt"
+
+if [ $? -eq 0 ]; then
+    testTwo="Pass"
+else
+    testTwo="Fail"
+fi
+echo -e "Finished Test 2 of 3 - Result is $testTwo"
+
+####################################################
+
+echo -e "\nRunning Test 3 of 3"
+
+if [ "`iw dev wlp2s0 link`" != "Not connected." ]; then
+    testThree="Pass"
+else
+    testThree="Fail"
 fi
 
-ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && conn="yes" || conn="no" &> $logLoc"/pingScr.txt"
+echo -e "Finished Test 3 of 3 - Result is $testThree"
 
-timeout --preserve-status 10 ping -vW 3 -w 3 www.google.com &> $logLoc"/ping.txt"
+#####################################################
 
-if [ "$?" == "0" ] && [ "$x" != "Not connected." ] && [ "$conn" = "yes" ] && [ "$spider" = "yes" ] && [ "$get" = "yes" ]
+if [ "$testOne" = "Pass" ] && [ "$testTwo" = "Pass" ] && [ "$testThree" = "Pass" ]
 then
-	echo -e "Looks like we are connected. Have a nice day\n"
+	echo -e "\nLooks like we are connected. Have a nice day\n"
 	exit 0
 fi
 
@@ -48,25 +65,18 @@ echo -e "Killing dhcpcd\n"
 sudo killall dhcpcd &> $logLoc"/killAllDh1.txt"
 echo -e "dhcpcd is killed\n"
 
+########################################################
+
 echo -e "Killing wpa_supplicant\n"
 sudo killall wpa_supplicant &> $logLoc"/killAllWpa.txt"
 echo -e "wpa_supplicant is killed"
 
 echo -e "\n--------------------------------------------"
 
-"/run/wpa_supplicant/wlp2s0" &> $logLoc"/runwlp2s0.txt"
-if [ "$?" != "127" ]
-then
-        echo -e "\nWe cant run wpa_supplicant. It seems that there is a file telling us that it is in use."
-        echo -e "Check to make sure that is not the case so that we can remove that file."
-        echo -e "\nPress any key to confirm."
-        read
-        sudo mv /run/wpa_supplicant/wlp2s0 $logLoc"/wlp2s0"
-        echo -e "That is done. wpa_supplicant should now work"
-fi
-
 echo -e "Restarting wpa_supplicant\n"
 sudo wpa_supplicant -B -i wlp2s0 -c /etc/wpa_supplicant/example.conf &> $logLoc"/wpaSupp1.txt"
+
+########################################################
 
 echo -e "\nRestarting dhcpcd\n"
 sudo dhcpcd -t 5 wlp2s0 &> $logLoc"/dhcpcd1.txt"
@@ -93,34 +103,51 @@ then
 fi
 
 echo -e "Now checking again if we are connected to the internet\n"
-x=`iw dev wlp2s0 link`
 
-wget -q --spider http://google.com &> $logLoc"/spider2.txt"
+###################################################
 
-if [ $? -eq 0 ]; then
-    spider="yes"
-else
-    spider="no"
-fi
-
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1 &> $logLoc"/getEcho2.txt"
+echo -e "\nRunning Test 1 of 3"
+timeout --preserve-status 4 wget -q --spider http://google.com &> $logLoc"/testOne2.txt"
 
 if [ $? -eq 0 ]; then
-    get="yes"
+    testOne="Pass"
 else
-    get="no"
+    testOne="Fail"
+fi
+echo -e "Finish Test 1 of 3 - Result is $testOne"
+
+###################################################
+
+echo -e "\nRunning Test 2 of 3"
+timeout --preserve-status 4 ping -vW 3 -w 3 www.google.com &> $logLoc"/testTwo.txt"
+
+if [ $? -eq 0 ]; then
+    testTwo="Pass"
+else
+    testTwo="Fail"
+fi
+echo -e "Finished Test 2 of 3 - Result is $testTwo"
+
+####################################################
+
+echo -e "\nRunning Test 3 of 3"
+
+if [ "`iw dev wlp2s0 link`" != "Not connected." ]; then
+    testThree="Pass"
+else
+    testThree="Fail"
 fi
 
-ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && conn="yes" || conn="no" &> $logLoc"/pingScr2.txt"
+echo -e "Finished Test 3 of 3 - Result is $testThree"
 
-timeout --preserve-status 10 ping -vW 3 -w 3 www.google.com &> $logLoc"/ping2.txt"
+#####################################################
 
-if [ "$?" == "0" ] && [ "$x" != "Not connected." ] && [ "$conn" = "yes" ] && [ "$spider" = "yes" ] && [ "$get" = "yes" ]
+if [ "$testOne" = "Pass" ] && [ "$testTwo" = "Pass" ] && [ "$testThree" = "Pass" ] 
 then
-        echo -e "\n--------------------------------------------"
-	echo -e "Looks like we are connected. Have a nice day\n"
+        echo -e "\nLooks like we are connected. Have a nice day\n"
         exit 0
 fi
+
 
 echo -e "\n--------------------------------------------"
 
